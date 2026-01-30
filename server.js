832cfe5d082eab3f3de518a6a340140f74357b42
Aman king <ry650506@gmail.com>
2026-01-30 20:49:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/bullion', (req, res) => {
 });
 
 // --- API: Get data for all assets ---
-app.get('/api/bullion-prices', (req, res) => {
+app.get('/api/bullion-prices', async (req, res) => {
     const cached = myCache.get("market_data");
     if (cached) return res.json(cached);
 
@@ -80,6 +80,7 @@ app.listen(PORT, '0.0.0.0', () => {
 
 
                                                            
+
 
 
 
